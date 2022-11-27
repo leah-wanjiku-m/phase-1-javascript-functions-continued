12e2c7fde380faf43e02a02ced1487b92c974d68
leah wanjiku <leagm22@gmail.com>
2022-11-27 05:54:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+return `This Saturday, I want to ${activity}!` ;
+}
+function  mondayWork(activity="go to the office"){
+  return `This Monday, I will ${activity}.`;
+}
+saturdayFun("bathe my dog");
+mondayWork("work from home");
+function wrapAdjective(wrap="*"){
+  const result=function(hard="a hard worker"){
+    return `You are ${wrap}${hard}${wrap}!`;
+  }
+  return result;
+}
